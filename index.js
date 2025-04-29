116cffec3943f4c9d6e8968c6ad21389d572e6a4
radiotron333 <mauriziodicristofaro@gmail.com>
2025-04-29 17:26:09 +0200

    index.js

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
   try {
-    const response = await fetch('http://air.doscast.com:8036/currentsong?sid=1');
+    res.set('Access-Control-Allow-Origin', '*'); // <<< QUESTA RIGA ABILITA CORS
+    const response = await fetch('http://air.doscast.com:8036/currentsong?sid=1', {
+      headers: {
+        'User-Agent': 'Mozilla/5.0'
+      }
+    });
     const titolo = await response.text();
     res.send(titolo);
   } catch (error) {
+    console.error(error);
     res.status(500).send('Errore nel recupero del titolo');
   }
 });
