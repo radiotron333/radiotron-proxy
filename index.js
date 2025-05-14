ddce0a24ceda4c74526310329960cb2759519949
radiotron333 <mauriziodicristofaro@gmail.com>
2025-05-14 14:50:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Relaxound - sostituisci l'URL con quello reale
-app.get("/relaxound", async (req, res) => {
+app.get("/dancenet", async (req, res) => {
   try {
     const resp = await fetch("http://air.doscast.com:8036/currentsong?sid=1");
     const text = await resp.text();
-    res.send(`Relaxound: ${text}`);
+    res.send(`${text}`);
   } catch (err) {
     res.status(500).send("Errore Relaxound: " + err.message);
   }
 });
 
 // Doscats
-app.get("/doscats", async (req, res) => {
+app.get("/relaxound", async (req, res) => {
   try {
     const resp = await fetch("http://air.doscats.com:8044/currentsong?sid=1");
     const text = await resp.text();
-    res.send(`Doscats: ${text}`);
+    res.send(`${text}`);
   } catch (err) {
     res.status(500).send("Errore Doscats: " + err.message);
   }
